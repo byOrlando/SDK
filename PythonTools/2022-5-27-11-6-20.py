331962c54f5db10b5fbf1ae58ad04a656f1a040d
Orlando <rakeecao@gmail.com>
2022-05-27 12:55:26 +0800

    2022-5-27-12-55-26 更新

--- PythonTools/2022-5-27-11-6-20.py
+++
@@ -1,1 +0,0 @@
-import timeimport time
--- ws.py
+++ ws.py
@@ -1,10 +1,12 @@
 import git
 import time
+from getStr import getStr
+
 timeData = time.localtime()
 fileName = f"{timeData.tm_year}-{timeData.tm_mon}-{timeData.tm_mday}-{timeData.tm_hour}-{timeData.tm_min}-{timeData.tm_sec}"
 
 with open(f"./PythonTools/{fileName}.py", 'a')as f:
-    f.write("import time")
+    f.write(getStr())
 
 
 repo = git.Repo(path='.')
